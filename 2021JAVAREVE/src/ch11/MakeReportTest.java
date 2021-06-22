06cd4c7a0004f4446d61020da68bc4c1079b0d94
haegun <maeguni1988@gmail.com>
2021-06-23 02:20:41 +0900

    20210623 java

---
+++ 2021JAVAREVE/src/ch11/MakeReportTest.java
@@ -0,0 +1,19 @@
+package ch11;
+
+public class MakeReportTest {
+	//Ä¸½¶È­ ¿¹Á¦
+	public static void main(String[] args) {
+		
+		
+		MakeReport builder = new MakeReport();
+		
+		String report = builder.getReport();
+		
+		
+		System.out.println(report);
+		
+	}
+	
+	
+
+}
