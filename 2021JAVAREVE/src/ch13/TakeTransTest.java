e20a3117766bb3b48c91f401095e8f74a041eb32
haegun <maeguni1988@gmail.com>
2021-06-25 23:23:02 +0900

    20210625 java

---
+++ 2021JAVAREVE/src/ch13/TakeTransTest.java
@@ -0,0 +1,34 @@
+package ch13;
+
+public class TakeTransTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Student studentJ = new Student("James", 5000);
+		Student studentT = new Student("Tomas", 10000);
+		
+		
+		Bus bus100 = new Bus(100);
+		Bus bus500 = new Bus(500);
+		
+		studentJ.takeBus(bus100);
+		
+		Subway greenSubway = new Subway(2);
+		studentT.takeSubway(greenSubway);
+		
+		studentJ.showInfo();
+		studentT.showInfo();
+		
+		bus100.showBusinfo();
+		greenSubway.showBusinfo();
+		bus500.showBusinfo();
+		
+		
+			
+		
+
+	}
+
+}
