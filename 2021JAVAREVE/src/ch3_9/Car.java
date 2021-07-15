f0738d32f06720646c8ea63c38fdc1540f6d44b9
haegun <maeguni1988@gmail.com>
2021-07-15 23:48:12 +0900

    20210715

---
+++ 2021JAVAREVE/src/ch3_9/Car.java
@@ -0,0 +1,20 @@
+package ch3_9;
+
+public abstract class Car {
+	
+	public abstract void drive();
+	public abstract void stop();
+	
+	
+	public void startCar() {
+		System.out.println("시동을 켭니다.");
+		
+	}
+
+	public void trunOff() {
+		System.out.println("시동을 끕니다.");
+		
+	}
+	
+
+}
