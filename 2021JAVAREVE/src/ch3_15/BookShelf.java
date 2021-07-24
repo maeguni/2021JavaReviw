8138b5cbc7a74520b612feecbd1e53e10bbb2010
haegun <maeguni1988@gmail.com>
2021-07-24 23:53:07 +0900

    20210724

---
+++ 2021JAVAREVE/src/ch3_15/BookShelf.java
@@ -0,0 +1,23 @@
+package ch3_15;
+
+public class BookShelf extends Shelf implements Queue{
+
+	@Override
+	public void enQueue(String title) {
+		shelf.add(title);
+		
+		
+	}
+
+	@Override
+	public String deQueue() {
+		return shelf.remove(0);
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return getCount();
+	}
+
+}
---
+++ 2021JAVAREVE/src/ch3_15/Queue.java
@@ -0,0 +1,11 @@
+package ch3_15;
+
+public interface Queue {
+	
+	void enQueue(String title);
+	String deQueue();
+	
+	int getSize();
+	
+
+}
