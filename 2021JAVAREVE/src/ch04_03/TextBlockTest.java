eb912a931b33149b78262e14ebb226cf5debd1d7
haegun <maeguni1988@gmail.com>
2021-08-02 23:51:53 +0900

    20210802

---
+++ 2021JAVAREVE/src/ch04_03/TextBlockTest.java
@@ -0,0 +1,16 @@
+package ch04_03;
+
+public class TextBlockTest {
+
+	public static void main(String[] args) {
+
+		String textBlocks = """
+				Hello,
+				hi,
+				how r u """;
+		
+		System.out.println(textBlocks);
+
+	}
+
+}
